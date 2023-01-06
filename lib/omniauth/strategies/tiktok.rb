9f73d11261a1303387cfd4968a1108fd20053812
Bruno Bornsztein <bruno.bornsztein@gmail.com>
2023-01-06 09:24:56 -0600

    remove redirect uri param

--- lib/omniauth/strategies/tiktok.rb
+++ lib/omniauth/strategies/tiktok.rb
@@ -1,56 +1,57 @@
 # frozen_string_literal: true
 
-require 'omniauth/strategies/oauth2'
+require "omniauth/strategies/oauth2"
 
 module OmniAuth
   module Strategies
     class Tiktok < OmniAuth::Strategies::OAuth2
       class NoAuthorizationCodeError < StandardError; end
-      DEFAULT_SCOPE = 'user.info.basic,video.list'
-      USER_INFO_URL = 'https://open-api.tiktok.com/oauth/userinfo'
 
-      option :name, 'tiktok'
+      DEFAULT_SCOPE = "user.info.basic,video.list"
+      USER_INFO_URL = "https://open-api.tiktok.com/oauth/userinfo"
+
+      option :name, "tiktok"
 
       option :client_options, {
-        site: 'https://open-api.tiktok.com',
-        authorize_url: 'https://open-api.tiktok.com/platform/oauth/connect',
-        token_url: 'https://open-api.tiktok.com/oauth/access_token',
+        site: "https://open-api.tiktok.com",
+        authorize_url: "https://open-api.tiktok.com/platform/oauth/connect",
+        token_url: "https://open-api.tiktok.com/oauth/access_token",
         extract_access_token: proc do |client, hash|
-          hash = hash['data']
-          token = hash.delete('access_token') || hash.delete(:access_token)
+          hash = hash["data"]
+          token = hash.delete("access_token") || hash.delete(:access_token)
           token && ::OAuth2::AccessToken.new(client, token, hash)
-        end
+        end,
       }
 
       option :authorize_options, %i[scope display auth_type]
 
-      uid { access_token.params['open_id'] }
+      uid { access_token.params["open_id"] }
 
       info do
-        prune!('nickname' => raw_info['data']['display_name'])
+        prune!("nickname" => raw_info["data"]["display_name"])
       end
 
       extra do
         hash = {}
-        hash['raw_info'] = raw_info unless skip_info?
+        hash["raw_info"] = raw_info unless skip_info?
         prune! hash
       end
 
       credentials do
         hash = {}
-        hash['token'] = access_token.token
-        hash['refresh_token'] = access_token.refresh_token if access_token.expires? && access_token.refresh_token
-        hash['expires_at'] = access_token.expires_at if access_token.expires?
-        hash['expires'] = access_token.expires?
-        refresh_token_expires_at = Time.now.to_i + access_token.params['refresh_expires_in'].to_i
-        hash['refresh_token_expires_at'] = refresh_token_expires_at
+        hash["token"] = access_token.token
+        hash["refresh_token"] = access_token.refresh_token if access_token.expires? && access_token.refresh_token
+        hash["expires_at"] = access_token.expires_at if access_token.expires?
+        hash["expires"] = access_token.expires?
+        refresh_token_expires_at = Time.now.to_i + access_token.params["refresh_expires_in"].to_i
+        hash["refresh_token_expires_at"] = refresh_token_expires_at
         hash
       end
 
       def raw_info
         @raw_info ||= access_token
-                      .get("#{USER_INFO_URL}?open_id=#{access_token.params['open_id']}&access_token=#{access_token.token}")
-                      .parsed || {}
+          .get("#{USER_INFO_URL}?open_id=#{access_token.params["open_id"]}&access_token=#{access_token.token}")
+          .parsed || {}
       end
 
       def callback_url
@@ -60,7 +61,7 @@ module OmniAuth
       def authorize_params
         super.tap do |params|
           params[:scope] ||= DEFAULT_SCOPE
-          params[:response_type] = 'code'
+          params[:response_type] = "code"
           params.delete(:client_id)
           params[:client_key] = options.client_id
         end
@@ -71,6 +72,17 @@ module OmniAuth
           params.delete(:client_id)
           params[:client_key] = options.client_id
         end
+      end
+
+      protected
+
+      def build_access_token
+        verifier = request.params["code"]
+        client.auth_code.get_token(
+          verifier,
+          token_params.to_hash(:symbolize_keys => true),
+          deep_symbolize(options.auth_token_params)
+        )
       end
 
       private
